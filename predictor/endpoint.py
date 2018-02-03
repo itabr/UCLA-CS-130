2a4d4c1cd1e13d493bd408b146af3d337adc7f20
Levi Vogel <levi@wifi-131-179-30-105.host.ucla.edu>
2018-02-03 15:08:01 -0800

    Added file separation and JSON compatibility

--- predictor/endpoint.py
+++ predictor/endpoint.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from .predict import DummyResponse
 app = Flask(__name__)
 
 def predict():
-	return "Nothing, because prediction is not implemented!"
+	resp = DummyResponse()
+	return resp.respond()
 
 @app.route('/', methods=['POST'])
 def responder():
+	req_data = request.get_json()
+	#print "Request JSON:"
+	#print req_data
 	response_str = '<h1>Your reponse is below:</h1><br>'
 	response_str += ("<p>" + predict() + "<p>")
-	return response_str
+	return jsonify({"prediction": str(predict())})
