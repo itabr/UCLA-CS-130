eebf733ac4dc774664f02ad49313c09289f07eb1
Levi Vogel <levi@Levis-MacBook-Air.local>
2018-02-09 16:52:15 -0800

    The endpoint now imports and uses a simple 'TagPredictor' class.  TagPredictor reads and unpickles an ML model (pre-trained) to make the prediction.  To pre-train the ML model, the 'data_grabbing', 'feature_generation', and 'learner' sets of scripts take training data, pre-process it into features, and train an ML model with the data features, then pickle the model for the TagPredictor.  A diagram of this process has been added to the team Drive.

--- endpoint.py
+++ endpoint.py
@@ -1,20 +1,11 @@
 from flask import Flask, request, jsonify
-#from .predict import DummyResponse
+from prediction_engine.predictor import TagPredictor
 app = Flask(__name__)
-
-class DummyResponse(object):
-	def respond(self):
-		return "Tag1 Tag2 Tag3"
-
-def predict():
-	resp = DummyResponse()
-	return resp.respond()
+predictor = TagPredictor("ridge/vsm", parameters_path = "prediction_engine/")
 
 @app.route('/', methods=['POST'])
 def responder():
 	req_data = request.get_json()
 	#print "Request JSON:"
 	#print req_data
-	response_str = '<h1>Your reponse is below:</h1><br>'
-	response_str += ("<p>" + predict() + "<p>")
-	return jsonify({"prediction": str(predict())})
+	return jsonify({"prediction": str(predictor.predict(req_data['data']))})
--- feature_generation/bagofwords/norm_tfidf_vsm.py
+++ feature_generation/bagofwords/norm_tfidf_vsm.py
@@ -3,36 +3,17 @@ import math
 import re
 import numpy
 import sys
+import multiprocessing
+import pickle
 
 if len(sys.argv) < 2:
-	exit("ERROR: Pass one argument to this program, the path of the file to analyze.")
-
-df = pandas.read_csv(sys.argv[1])
-labels = df['labels']
-prob_statements = df['problem-statement']
+	exit("ERROR: Pass one argument to this program, the path of the data file.")
 
 def string_to_words(input_str):
 	cleaned_str = re.sub("[^abcdefghijklmnopqrstuvwxyz'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890\ ]", ' ', input_str)
 	return filter(lambda w: len(w) > 0, cleaned_str.split(" "))
 
-word_counts = {}
-for statement in prob_statements:
-	try:
-		words = string_to_words(statement)
-	except:
-		continue
-	uniques = set(words)
-	for word in uniques:
-		try:
-			word_counts[word] += 1
-		except:
-			word_counts[word] = 1
-idf = {}
-num_probs = len(prob_statements)
-for key in word_counts:
-	idf[key] = math.log10(num_probs / word_counts[key])
-
-def get_counts(words_arr):
+def get_word_counts(words_arr):
 	ret = {}
 	for word in words_arr:
 		try:
@@ -41,25 +22,56 @@ def get_counts(words_arr):
 			ret[word] = 1
 	return ret
 
-def get_vsm(input_str):
+def norm_magnitude_vector(input_vec):
+	sum_squares = sum(map(lambda x: x**2, input_vec))
+	magnitude = sum_squares**.5
+	if magnitude == 0:
+		return [0 for i in input_vec]
+	return list(map(lambda x: x / magnitude, input_vec))
+
+df = pandas.read_csv(sys.argv[1])
+labels = df['labels']
+prob_statements = df['problem-statement']
+
+document_frequency = {}
+for statement in prob_statements:
+	try:
+		words = string_to_words(statement)
+	except:
+		continue
+	uniques = set(words)
+	for word in uniques:
+		try:
+			document_frequency[word] += 1
+		except:
+			document_frequency[word] = 1
+idf = {}
+num_docs = len(prob_statements)
+for key in document_frequency:
+	idf[key] = math.log10(num_docs / document_frequency[key])
+
+def get_document_vector(input_str):
 	try:
 		words = string_to_words(input_str)
 		if len(words) == 0:
-			return [0 for word in word_counts]
+			return [0 for word in idf]
 	except:
-		return [0 for word in word_counts]
+		return [0 for word in idf]
 
-	counts = get_counts(words)
-	un_normed_vsm = [counts[word] * idf[word] if word in counts else 0 for word in idf]
-	tot = un_normed_vsm[0]**2 - un_normed_vsm[0] + reduce((lambda x, y: x + y**2), un_normed_vsm)
-	factor = tot**.5
-	return list(map(lambda x: x / factor, un_normed_vsm))
+	counts = get_word_counts(words)
+	un_normed_vec = [counts[word] * idf[word] if word in counts else 0 for word in idf]
+	return norm_magnitude_vector(un_normed_vec)
 
-features = list(map(get_vsm, prob_statements))
+pool = multiprocessing.Pool()
+vsm_features = list(pool.map(get_document_vector, prob_statements))
 df_columns = [word for word in idf]
-output_df = pandas.DataFrame(data=features, columns=df_columns)
+output_df = pandas.DataFrame(data=vsm_features, columns=df_columns)
 output_df['FEATURE_LABELS'] = labels
 
 filename = sys.argv[1].split("/")
 filename = "".join(filename[len(filename)-1].split(".")[:1])
 output_df.to_csv(filename + "_features.csv", index=False)
+
+f = open('idf_dump.txt', 'w')
+f.write(pickle.dumps(idf))
+f.close()
