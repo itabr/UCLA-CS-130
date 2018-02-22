bdb4275cbcbb3158b1990891058bef0c82ffe579
Levi Vogel <levi@Levis-MacBook-Air.local>
2018-02-22 14:49:41 -0800

    created a small test suite, and modified web app for robustness

--- endpoint.py
+++ endpoint.py
@@ -6,6 +6,20 @@ predictor = TagPredictor("ridge/vsm", parameters_path = "prediction_engine/")
 @app.route('/', methods=['POST'])
 def responder():
 	req_data = request.get_json()
-	#print "Request JSON:"
-	#print req_data
-	return jsonify({"prediction": str(predictor.predict(req_data['data']))})
+	try:
+		if req_data['test'] == 200 or int(req_data['test']) == 200:
+			return jsonify({'test': 'all clear'})
+	except:
+		pass
+	try:
+		prob_st = req_data['data']
+	except:
+		return jsonify({'error': "incorrect input format: input key must be 'data', and input value must be a string"}), 400
+	try:
+		if type(prob_st) is str or type(prob_st) is unicode:
+			return jsonify({"prediction": str(predictor.predict(prob_st))})
+		else:
+			return jsonify({'error': 'incorrect input format: input value must be a string'}), 400
+	except Exception, e:
+		return jsonify({'error': 'internal server error: ' + str(e)}), 500
+		
