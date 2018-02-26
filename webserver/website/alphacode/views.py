52fe300529d5b094c1d7172e9bd9005982b7208f
Wenhui Kuang <wanykuang@gmail.com>
2018-02-25 17:12:46 -0800

    Can generate random URL with mix of string and digit

--- webserver/website/alphacode/views.py
+++ webserver/website/alphacode/views.py
@@ -19,20 +19,27 @@ def index(request):
 
 #create/
 def create(request):
-    groupName = "alphacode"
+    groupName = request.GET.get('name')
     N = 16
-    random_string = ''.join(random.choice(string.digits) for _ in range(N))
+    while True:
+        random_string = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(N))
+        try:
+            row = RandomURLs.objects.get(random_url=random_string)
+        except RandomURLs.DoesNotExist:
+            break  
+    Row = RandomURLs(random_url=random_string, group_name=groupName, timestamp=timezone.now(), valid=True)
+    Row.save()
     return redirect(workplace, workplace_id=random_string)
 
 #alphacode/(workplace_id)
 def workplace(request,workplace_id):
-    # try:
-    #     groupName = RandomURLs.objects.get(random_url=workplace_id)
-    # except RandomURLs.DoesNotExist:
-    #     return redirect('error-view')
+    try:
+        groupName = RandomURLs.objects.get(random_url=workplace_id)
+    except RandomURLs.DoesNotExist:
+        return redirect('error-view')
     context = {
         "workplace_id": workplace_id,
-        # "group_name"  : groupName.group_name,
+        "group_name"  : groupName.group_name,
     }
     return render(request,'alphacode/workplace.html',context)
 
