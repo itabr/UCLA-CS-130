fc4f7f57301ad2dd5db9843194f289032cbd6fc7
Levi Vogel <levi@Levis-MacBook-Air.local>
2018-03-11 19:59:44 -0700

    re-wrote 'predictor.py' to incorporate many models simultaneously

--- predictor/prediction_engine/predictor.py
+++ predictor/prediction_engine/predictor.py
@@ -1,67 +1,83 @@
 import json
 import pickle
 import re
+import pandas
+import keras
 from sklearn.linear_model import RidgeClassifierCV
-from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from keras.preprocessing.text import hashing_trick
+from keras.models import load_model
 
-class VSMSampleMapper(object):
-	def __init__(self, prefix = "", idf_file_type = 'pickle'):
-		if idf_file_type == "pickle":
-			f = open(prefix + "idf.txt")
-			self.idf = pickle.loads(f.read())
-			f.close()
-		else:
-			f = open(prefix + "idf.txt")
-			self.idf = json.loads(f.read())
-			f.close()
+tfidf_labels = ["math", "dp", "geometry", "combinatorics", "datastructures",
+	"games", "graphs", "probabilities", "sortings", "strings", "twopointers"]
+nn_labels = ["implementation", "search", "greedy"] # games? numbertheory? strings?
+hashing_trick_max_features = 20000
+nn_features_max_size = 5000
+confidence_threshold = .5
 
-	def _string_to_words(self, input_str):
-		cleaned_str = re.sub("[^abcdefghijklmnopqrstuvwxyz'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890\ ]", ' ', input_str)
-		return filter(lambda w: len(w) > 0, cleaned_str.split(" "))
 
-	def _get_word_counts(self, words_arr):
-		ret = {}
-		for word in words_arr:
-			try:
-				ret[word] += 1
-			except:
-				ret[word] = 1
-		return ret
+class TFIDFMapper(object):
+	def __init__(self, prefix):
+		data = pandas.read_csv(prefix + 'data.csv')
+		data = data.dropna()
 
-	def _norm_magnitude_vector(self, input_vec):
-		sum_squares = sum(map(lambda x: x**2, input_vec))
-		magnitude = sum_squares**.5
-		if magnitude == 0:
-			return [0 for i in input_vec]
-		return list(map(lambda x: x / magnitude, input_vec))
+		vectorizer = TfidfVectorizer(stop_words="english", analyzer = 'word')
+		l = (data['input-spec'].astype(str) + (data['problem-statement'].astype(str) + data['output-spec'])) .tolist()
+		res = []
+		self.regex = re.compile("^a-zA-Z0-9\ '" )
+		for i in range(len(l)):
+		    try:
+		        res.append(self.regex.sub(' ', l[i]))
+		    except:
+		        res.append("")
+		self.vectorizer.fit(res)
 
 	def map_sample(self, sample):
-		word_counts = self._get_word_counts(self._string_to_words(sample))
-		conversion = [word_counts[word] * self.idf[word] if word in word_counts else 0 for word in self.idf]
-		return self._norm_magnitude_vector(conversion)
+		return self.vectorizer.transform([sample])
 
-class RidgeCLF(object):
-	def __init__(self, prefix = ""):
-		f = open(prefix + "ridge_clf.txt")
+
+class NNMapper(object):
+	def map_sample(self, sample):
+		features = hashing_trick(sample, hashing_trick_max_features)
+		return features + [0] * (nn_features_max_size - len(features))
+
+
+class SKLClassifier(object):
+	def __init__(self, name, prefix="", mapper_fn):
+		f = open(prefix + name + ".dump")
 		self.clf = pickle.loads(f.read())
 		f.close()
-		f = open(prefix + "binarizer.txt")
-		self.binarizer = pickle.loads(f.read())
-		f.close()
-
-	def predict(self, sample_vec):
-		pred = self.clf.predict([sample_vec])[0]
-		return self.binarizer.classes_[int(pred)]
-
-class TagPredictor(object):
-	def __init__(self, algorithm = "ridge/vsm", parameters_path = False):
-		if parameters_path:
-			self.mapper = VSMSampleMapper(prefix = parameters_path)
-			self.predictor = RidgeCLF(prefix = parameters_path)
-		else:
-			self.mapper = VSMSampleMapper()
-			self.predictor = RidgeCLF()
+		self.mapper_fn = mapper_fn
 
 	def predict(self, sample):
-		sample_features = self.mapper.map_sample(sample)
-		return self.predictor.predict(sample_features)
+		return self.clf.predict(self.mapper_fn(sample))
+
+
+class KerasClassifier(object):
+	def __init__(self, name, prefix="", mapper_fn):
+		self.clf = load_model(prefix + name + ".dump")
+		self.mapper_fn = mapper_fn
+
+	def predict(self, sample):
+		return self.clf.predict(self.mapper_fn(sample))
+
+
+class TagPredictor(object):
+	def __init__(self, parameters_path = ""):
+		self.tfidf_mapper = TFIDFMapper(prefix = parameters_path)
+		self.nn_mapper = NNMapper(prefix=parameters_path)
+
+		self.label_classifiers = {}
+		for label in tfidf_labels:
+			self.label_classifiers[label] = SKLClassifier(label, parameters_path, self.tfidf_mapper.map_sample)
+		for label in nn_labels:
+			self.label_classifiers[label] = KerasClassifier(label, parameters_path, self.nn_mapper.map_sample)
+
+	def predict(self, sample):
+		predictions = []
+
+		for label in self.label_classifiers:
+			if self.label_classifiers[label].predict(sample) >= confidence_threshold:
+				predictions.append(label)
+
+		return predictions
