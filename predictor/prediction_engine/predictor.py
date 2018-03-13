c17951d9d5998fb1de74320587c795f9d9aa0134
Levi Vogel <levi@Levis-MacBook-Air.local>
2018-03-12 20:20:55 -0700

    modified 'predictor.py' to use 'joblib' instead of 'pickle'

--- predictor/prediction_engine/predictor.py
+++ predictor/prediction_engine/predictor.py
@@ -7,33 +7,8 @@ import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from keras.preprocessing.text import hashing_trick
 from keras.models import load_model
+from sklearn.externals import joblib
 
-from sklearn import linear_model
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
-
-from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
-from sklearn import svm
-
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-
-from sklearn.naive_bayes import GaussianNB
-
-import nltk
-from nltk import word_tokenize          
-from nltk.stem.porter import PorterStemmer
-
-from sklearn.model_selection import GridSearchCV
 
 tfidf_labels = ["math", "dp", "datastructure", "greedy", "graphs", "probabilities", "sortings", "strings", "numbertheory"]
 nn_labels = ["combinatorics", "games", "geometry", "search"]
@@ -70,9 +45,7 @@ class NNMapper(object):
 
 class SKLClassifier(object):
 	def __init__(self, name, mapper_fn, prefix=""):
-		f = open(prefix + name + ".dump")
-		self.clf = pickle.loads(f.read())
-		f.close()
+		self.clf = joblib.load(prefix + name + ".dump")
 		self.mapper_fn = mapper_fn
 
 	def predict(self, sample):
@@ -96,15 +69,9 @@ class TagPredictor(object):
 		self.label_classifiers = {}
 		failure = False
 		for label in tfidf_labels:
-			try:
-				self.label_classifiers[label] = SKLClassifier(label, self.tfidf_mapper.map_sample, parameters_path)
-				print "SKL Classifier Loaded: " + label
-			except Exception, e:
-				failure = True
-				print "SKL Classifier Load Failed: " + label + ";     " + str(e)
+			self.label_classifiers[label] = SKLClassifier(label, self.tfidf_mapper.map_sample, parameters_path)
 		for label in nn_labels:
 			self.label_classifiers[label] = KerasClassifier(label, self.nn_mapper.map_sample, parameters_path)
-		exit("done")
 
 	def predict(self, sample):
 		predictions = []
